39f42d1f893aff649ecb1efd9cbf3d1f2f6e1845
Nozdri <realnozdri@gmail.com>
2023-07-19 18:47:18 +0300

    test packagist auto-update

--- src/Posting.php
+++ src/Posting.php
@@ -2,5 +2,12 @@
 
 abstract class Posting
 {
+    public function __construct(protected array $data)
+    {
+    }
 
+    public function getPostingNumber(): string
+    {
+        return $this->data['posting_number'];
+    }
 }
